83c0734d9d6d2d8e12fcce6b1205d309f3ca14bb
GwillCheese <danielkaku555@gmail.com>
2026-01-05 19:59:21 -0700

    Setup Prisma v7, connect PostgreSQL, initial User and Application models, add dependencies

---
+++ prisma/migrations/20260106021845_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Application" (
+    "id" SERIAL NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "jobTitle" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
